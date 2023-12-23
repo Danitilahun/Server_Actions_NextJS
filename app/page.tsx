801226d72cc606e101406f5bc93a4fdc4d56cc87
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 23:38:54 +0300

    Adding toaster and create form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+import CreateForm from "@/component/create-form";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div className="mx-auto max-w-2xl lg:max-w-7xl">
+      <div className="flex justify-between items-center">
+        <h1 className="font-bold py-10 text-2xl">Admin Products</h1>
+        <Toaster />
+        <CreateForm />
+      </div>
+    </div>
   );
 }
