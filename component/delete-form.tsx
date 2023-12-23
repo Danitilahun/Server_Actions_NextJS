e878586b7296fb4e8f007824daee862d526233d1
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 23:25:36 +0300

    finish delete form

--- component/delete-form.tsx
+++ component/delete-form.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { deleteProduct } from "@/lib/actions";
 import { useFormStatus } from "react-dom";
+import toast from "react-hot-toast";
 
 export default function DeleteForm({
   _id,
@@ -8,4 +12,18 @@ export default function DeleteForm({
   name: string;
 }) {
   const { pending } = useFormStatus();
+  return (
+    <form
+      action={async (formData) => {
+        const res = await deleteProduct(formData);
+        toast(res.message);
+      }}
+    >
+      <input type="hidden" name="_id" value={_id} />
+      <input type="hidden" name="name" value={name} />
+      <button type="submit" disabled={pending} className="btn btn-ghost">
+        Delete
+      </button>
+    </form>
+  );
 }
