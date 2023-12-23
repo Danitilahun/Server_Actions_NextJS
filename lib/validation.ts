c712bb644a5880c6c2f64200bd77fb96173eb483
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 22:44:47 +0300

    Adding validation schema for product using zod

---
+++ lib/validation.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const schema = z.object({
+  name: z.string().min(3),
+  image: z.string().min(1),
+  price: z.number().min(1),
+  rating: z.number().min(1).max(5),
+});
