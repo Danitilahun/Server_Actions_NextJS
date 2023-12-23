78b42d9323677e3b4ca19cd958715d3d8c05b959
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 23:23:46 +0300

    use form status to show pending

--- component/delete-form.tsx
+++ component/delete-form.tsx
@@ -1,7 +1,11 @@
+import { useFormStatus } from "react-dom";
+
 export default function DeleteForm({
   _id,
   name,
 }: {
   _id: string;
   name: string;
-}) {}
+}) {
+  const { pending } = useFormStatus();
+}
