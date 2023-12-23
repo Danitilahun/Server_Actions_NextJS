8106c209e57641d1edfb9542cb5a1bf4a64cd04e
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 23:45:42 +0300

    Adding table

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import CreateForm from "@/component/create-form";
+import DeleteForm from "@/component/delete-form";
+import Rating from "@/component/rating";
+import dbConnect from "@/lib/db-connect";
+import ProductModel, { Product } from "@/lib/product-model";
+import Image from "next/image";
 import { Toaster } from "react-hot-toast";
 
-export default function Home() {
+export default async function Home() {
+  await dbConnect();
+  const products = (await ProductModel.find({}).sort({
+    _id: -1,
+  })) as Product[];
   return (
     <div className="mx-auto max-w-2xl lg:max-w-7xl">
       <div className="flex justify-between items-center">
@@ -9,6 +18,49 @@ export default function Home() {
         <Toaster />
         <CreateForm />
       </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Rating</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.length === 0 ? (
+            <tr>
+              <td colSpan={5}>No product found</td>
+            </tr>
+          ) : (
+            products.map((product: Product) => (
+              <tr key={product._id}>
+                <td>
+                  <Image
+                    src={product.image}
+                    alt={product.name}
+                    width={80}
+                    height={80}
+                    className="rounded-lg"
+                  />
+                </td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>
+                  <Rating value={product.rating} />
+                </td>
+                <td>
+                  <DeleteForm
+                    _id={product._id.toString()}
+                    name={product.name}
+                  />
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
