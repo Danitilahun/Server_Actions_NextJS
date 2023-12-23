bbb5d806ba94a2221bda3f9c48ed3013f5c049a3
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 23:08:59 +0300

    Create useeffect that runs based on the state to clonse the model and reset the form

--- component/create-form.tsx
+++ component/create-form.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { createProduct } from "@/lib/actions";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useFormState, useFormStatus } from "react-dom";
+import toast from "react-hot-toast";
 
 export default function CreateForm() {
   const [state, formAction] = useFormState(createProduct, {
@@ -11,5 +12,15 @@ export default function CreateForm() {
 
   const { pending } = useFormStatus();
   const ref = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    if (state.message.indexOf("Created product") === 0) {
+      (document.getElementById("my_modal_3") as any)!.close();
+      ref.current?.reset();
+      toast(state.message);
+    } else if (state.message) {
+      toast(state.message);
+    }
+  }, [state.message]);
   return <div></div>;
 }
