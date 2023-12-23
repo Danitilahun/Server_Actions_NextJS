3f58154ddc6bdf541e6b33c0d273fd2f93ebb558
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 23:21:06 +0300

    Adding dialog box with form

--- component/create-form.tsx
+++ component/create-form.tsx
@@ -32,6 +32,62 @@ export default function CreateForm() {
       >
         Create Product
       </button>
+
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <h2 className="tex-2xl font-bold pm-4">Create Product</h2>
+          <form ref={ref} action={formAction}>
+            <div className="form-control w-full max-w-xs py-4">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="input input-bordered w-full max-w-xs"
+                required
+              />
+            </div>
+            <div className="form-control w-full max-w-xs py-4">
+              <label htmlFor="image">Image</label>
+              <input
+                type="text"
+                id="image"
+                name="image"
+                className="input input-bordered w-full max-w-xs"
+                required
+                defaultValue="/images/shirt1.jpg"
+              />
+            </div>
+            <div className="form-control w-full max-w-xs py-4">
+              <label htmlFor="price">Price</label>
+              <input
+                type="number"
+                id="price"
+                name="price"
+                className="input input-bordered w-full max-w-xs"
+                required
+                defaultValue="1"
+              />
+            </div>
+            <button
+              className="btn btn-primary mr-3"
+              type="submit"
+              disabled={pending}
+            >
+              Create
+            </button>
+            <button
+              type="button"
+              className="btn btn-ghost"
+              onClick={() =>
+                (document.getElementById("my_modal_3") as any).close()
+              }
+            >
+              Back
+            </button>
+          </form>
+        </div>
+      </dialog>
     </div>
   );
 }
