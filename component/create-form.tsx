3736299e45ed6d2458f08faad569fe3e57762164
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 23:04:47 +0300

    using useformstate hook and create product as an action

--- component/create-form.tsx
+++ component/create-form.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { createProduct } from "@/lib/actions";
+import { useFormState } from "react-dom";
+
 export default function CreateForm() {
+  const [state, formAction] = useFormState(createProduct, {
+    message: "",
+  });
   return <div></div>;
 }
