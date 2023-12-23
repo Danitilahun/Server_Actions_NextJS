c1c0a4ac23591b42807eb7f685efedc5682eab92
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 23:05:30 +0300

    use useformstatus hook to show pending status

--- component/create-form.tsx
+++ component/create-form.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { createProduct } from "@/lib/actions";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 
 export default function CreateForm() {
   const [state, formAction] = useFormState(createProduct, {
     message: "",
   });
+
+  const { pending } = useFormStatus();
   return <div></div>;
 }
