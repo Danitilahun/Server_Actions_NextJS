4d2b293860b35747b95ec611f9b37f28cd2d66ff
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 22:49:56 +0300

    Adding create product function and then safeparse the formData to cross check it with the validation schema

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,4 +2,13 @@
 import { revalidatePath } from "next/cache";
 import ProductModel from "./product-model";
 import dbConnect from "./db-connect";
-import { z } from "zod";
+import { schema } from "./validation";
+
+export async function createProduct(prevState: any, formData: FormData) {
+  const parse = schema.safeParse({
+    name: formData.get("name"),
+    image: formData.get("image"),
+    price: Number(formData.get("price")),
+    rating: Math.ceil(Math.random() * 5),
+  });
+}
